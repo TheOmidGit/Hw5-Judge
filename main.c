305eaa02d818f4ed3b83170fe697501120f23d67
AmirAG80 <56169125+AmirAG80@users.noreply.github.com>
2020-06-20 21:26:11 +0430

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,61 @@
-#include <stdio.h>
-
 #include "generator/generator.h"
 #include "judge/judge.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BACK 2
+
+int generateTestCase(){
+    int result;
+    //======================
+    result = compile();
+    if (result == BACK){
+        return BACK;
+    }
+    //======================
+    result = getTestsPath();
+    if (result == BACK){
+        return BACK;
+    }
+    //======================
+    generate();
+    return 1;
+    
+}
+
+int judge(){
+    int result;
+    result = system("FC \"output1.txt\" \"output2.txt\"");
+    printf("%d",result);getchar();
+}
 
 int main() {
+    while (1)
+    {   
+        int result;
+        system("cls");
+        //menu
+        printf("1 -> Generate test case\n2 -> Judge\n3 -> Exit\n");
+        int choice;
+        scanf("%d",&choice);getchar();
 
-    // use your modules here
-
+        switch (choice)
+        {
+        case 1:
+            system("cls");
+            generateTestCase();
+            break;
+        case 2:
+            judge();
+            break;
+        case 3:
+            return 0;
+        default:
+            break;
+        }
+        
+    }
+    
     return 0;
 }
